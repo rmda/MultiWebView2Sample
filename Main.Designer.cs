986a585117ce3b450428d65e684d2afdf16104e6
rmda <rmda@users.noreply.github.com>
2021-06-17 18:45:50 -0500

    Initial commit

---
+++ Main.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace MultiWebView2Sample
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.createButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(647, 12);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(141, 23);
+            this.createButton.TabIndex = 0;
+            this.createButton.Text = "Create new WebView2";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.createButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Main Window";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createButton;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Core;
+
+
+namespace MultiWebView2Sample
+{
+    public partial class Main : Form
+    {
+        CoreWebView2Environment env;
+        string userDataFolder;
+        int browserCount = 0;
+        int browsersPerRow = 4;
+
+        public Main()
+        {
+            InitializeComponent();
+            userDataDirectorySetup();
+            webView2Setup();
+        }
+
+        private void userDataDirectorySetup()
+        {
+            Debug.Print("Entering userDataDirectorySetup.. ");
+            string userprofile = Environment.GetEnvironmentVariable("USERPROFILE");
+            userDataFolder = $"{userprofile}\\AppData\\Local\\MultiWebView2Sample\\";
+
+            if (!Directory.Exists(userDataFolder))
+            {
+                DirectoryInfo di = Directory.CreateDirectory(userDataFolder);
+                Debug.Print($"Created directory {userDataFolder}");
+            }
+
+            Debug.Print("Leaving userDataDirectorySetup.. ");
+        }
+
+        async private void webView2Setup()
+        {
+            Debug.Print("Entering webView2Setup.. ");
+            env = await CoreWebView2Environment.CreateAsync(null, userDataFolder, null);
+            /*
+             *  CreateAsync( 
+             *      browserExecutableFolder should remain null, since you should be running this from Visual Studio
+             *      userDataFolder should be "C:\\Users\\username\\AppData\\Local\\MultiWebView2Sample\\" or similar, once WebView2 is created programmatically it will create an EBWebView folder
+             *      options should be null, but you can even specify some and test this out. It should use the same instance over a 'null' value as long as we're using the same environment.
+             *      
+             */
+
+            //CoreWebView2EnvironmentOptions opts;
+            //opts.AdditionalBrowserArguments = "";
+            //opts.AllowSingleSignOnUsingOSPrimaryAccount = true;
+            //opts.Language = "es-US";
+            //opts.TargetCompatibleBrowserVersion = true;
+
+            Debug.Print($"Created env {env}");
+
+            Debug.Print("Leaving webView2Setup.. ");
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            Debug.Print("Entering createButton_Click..");
+            createWebView2();
+            Debug.Print("Leaving createButton_Click..");
+        }
+
+        async private void createWebView2()
+        {
+            Debug.Print("Entering makeBrowser..");
+            WebView2 wv2 = new WebView2();
+            wv2.Width = 200;
+            wv2.Height = 200;
+            wv2.Left = 201 * (browserCount % browsersPerRow);
+            wv2.Top = 201 * (browserCount / browsersPerRow);
+
+            Debug.Print($"{wv2} added to Main form");
+
+            await wv2.EnsureCoreWebView2Async(env);
+            // If you pass null (the default value) then a default environment will be created and used automatically.
+            // from  https://docs.microsoft.com/en-us/dotnet/api/microsoft.web.webview2.winforms.webview2.ensurecorewebview2async?view=webview2-dotnet-1.0.864.35#parameters 
+
+            Controls.Add(wv2);
+            wv2.CoreWebView2.Navigate(@"https://www.bing.com");
+
+            browserCount++;
+
+            Debug.Print($"Leaving makeBrowser, another WebView2 added: {browserCount} total");
+        }
+    }
+}
